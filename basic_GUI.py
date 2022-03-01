190a894777ce380ff42465579afe3c16e8f11777
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2022-02-28 22:59:12 -0500

    CreatedAssignments?

--- basic_GUI.py
+++ basic_GUI.py
@@ -11,8 +11,9 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "NAME IN PROGRESS"
 
 STAT_NAMES = ["grade","sleep","happiness","work_ethic","fun"]
-
-SCENE_COLOR = [arcade.color.AIR_FORCE_BLUE, arcade.color.ANTIQUE_BRONZE]
+DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturn"]
+# Colors for all of the times
+TIME_COLORS = [arcade.color.AERO_BLUE,arcade.color.AFRICAN_VIOLET,arcade.color.AIR_FORCE_BLUE]
 # Rectangle info
 
 class Rect:
@@ -56,15 +57,22 @@ class GameView(arcade.View):
         self.act_rects = []
         self.act_labels = []
         self.act_buttons = []
+        self.assign_open = False
+        self.assign_rects = []
+        self.assign_labels = []
+        self.assign_buttons = []
         # --- Required for all code that uses UI element,
         # a UIManager to handle the UI.
+        
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
         self.rect_list = []
         self.top_list = []
         self.labels = []
+        
+        self.last_time = 0
         #time
-        self.total_time = 0.0
+       
         self.output = "00:00:00"
     
 
@@ -74,6 +82,7 @@ class GameView(arcade.View):
 
         # # Create our rectangle
         self.lower_frame = Rect(SCREEN_WIDTH, SCREEN_HEIGHT/3, SCREEN_WIDTH/2, SCREEN_HEIGHT/6, arcade.color.MSU_GREEN)
+        self.time_frame = Rect(90, 50, 65, SCREEN_HEIGHT-35, arcade.color.ROYAL_PURPLE)
         self.teacher = Rect(SCREEN_WIDTH/5, SCREEN_HEIGHT/2, SCREEN_WIDTH/2,SCREEN_HEIGHT/2,arcade.color.RED_DEVIL)
         
         self.stats_button = arcade.gui.UIFlatButton(text="Stats",
@@ -143,6 +152,50 @@ class GameView(arcade.View):
                 child=self.act_buttons_box)
         )
 
+        self.assign_box = arcade.gui.UIBoxLayout()
+
+
+        self.assign_button = arcade.gui.UIFlatButton(text="ASSIGNMENTS",
+                                               width=200)
+        self.assign_button.on_click = self.make_assign
+        self.assign_box.add(self.assign_button)
+
+
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="right",
+                anchor_y="bottom",
+                align_x= -40,
+                align_y= 40,
+                
+                child=self.assign_box)
+        )
+
+      
+
+        # Buttons Section
+
+        self.assign_buttons_box = arcade.gui.UIBoxLayout()
+
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="left",
+                anchor_y="center",
+                align_x=175,
+                y=30,
+                child=self.assign_buttons_box)
+        )
+
+        self.assign_muttons_box = arcade.gui.UIBoxLayout()
+
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="right",
+                anchor_y="center",
+                y=30,
+                child=self.assign_muttons_box)
+        )
+
         self.sleep_button = arcade.gui.UIFlatButton(text="Sleep", width=200) 
         self.sleep_button.on_click = lambda event : self.act(event,0)
 
@@ -175,6 +228,59 @@ class GameView(arcade.View):
 
         self.ask_for_help_button = arcade.gui.UIFlatButton(text="Ask For Help", width=200) 
         self.ask_for_help_button.on_click = lambda event : self.act(event,1)
+
+        self.assignments = {}
+        self.assignments_done = {}
+
+        # Tried to make this a for loop, couldn't get it to work and I am tired
+        # self.assignment_list = [(1,9),(1,15),(2,10),(2,14),(2,15),(3,14),(4,8),(4,13)]
+        
+        assignment0 = arcade.gui.UIFlatButton(text="(0,20)", width=200) 
+        assignment0.on_click = lambda event : self.do_assignment(event,(0,20))
+
+        self.assignments[(0,20)] = assignment0
+
+        assignment1 = arcade.gui.UIFlatButton(text="(1,9)", width=200) 
+        assignment1.on_click = lambda event : self.do_assignment(event,(1,9))
+
+        self.assignments[(1,9)] = assignment1
+
+        assignment2 = arcade.gui.UIFlatButton(text="(1,15)", width=200) 
+        assignment2.on_click = lambda event : self.do_assignment(event,(1,15))
+
+        self.assignments[(1,15)] = assignment2
+
+        assignment3 = arcade.gui.UIFlatButton(text="(2,10)", width=200) 
+        assignment3.on_click = lambda event : self.do_assignment(event,(2,10))
+
+        self.assignments[(2,10)] = assignment3
+
+        assignment34 = arcade.gui.UIFlatButton(text="(2,14)", width=200) 
+        assignment34.on_click = lambda event : self.do_assignment(event,(2,14))
+
+        self.assignments[(2,14)] = assignment34
+
+        assignment4 = arcade.gui.UIFlatButton(text="(2,15)", width=200) 
+        assignment4.on_click = lambda event : self.do_assignment(event,(2,15))
+
+        self.assignments[(2,15)] = assignment4
+
+        assignmentA = arcade.gui.UIFlatButton(text="(3,14)", width=200) 
+        assignmentA.on_click = lambda event : self.do_assignment(event,(3,14))
+
+        self.assignments[(3,14)] = assignmentA
+
+        assignment5 = arcade.gui.UIFlatButton(text="(4,8)", width=200) 
+        assignment5.on_click = lambda event : self.do_assignment(event,(4,8))
+
+        self.assignments[(4,8)] = assignment5
+
+        assignment45 = arcade.gui.UIFlatButton(text="(4,13)", width=200) 
+        assignment45.on_click = lambda event : self.do_assignment(event,(4,13))
+
+        self.assignments[(4,13)] = assignment45
+
+        self.create_message("Welcome to your first day at BCA!\nYou should probably get some sleep for your first day!")
 
 
         
@@ -259,6 +365,8 @@ class GameView(arcade.View):
         else:
             if self.act_open:
                 self.make_act(event)
+            if self.assign_open:
+                self.make_assign(event)
             self.stats_rect_main = Rect(500,400,SCREEN_WIDTH/2,SCREEN_HEIGHT/2, arcade.color.PURPLE_HEART)
             self.stats_rects.append(self.stats_rect_main)
             self.rectangle_appear(self.stats_rect_main)
@@ -285,18 +393,20 @@ class GameView(arcade.View):
     def on_update(self, delta_time):
         # Move the rectangle
         
-        if self.isGoing:
-            arcade.set_background_color((self.color[0],self.color[1],self.color[2]))
-            if self.color[self.last_max] > 50:
-                self.color[self.last_max] -=2
-            if self.color[((self.last_max+1) % 3)] <200:
-                self.color[((self.last_max+1) % 3)] += 2
-            else:
-                self.last_max +=1
-                self.last_max %=3
-        if len(self.top_list):
-            print(self.top_list)
-        
+        # if self.isGoing:
+        #     arcade.set_background_color((self.color[0],self.color[1],self.color[2]))
+        #     if self.color[self.last_max] > 50:
+        #         self.color[self.last_max] -=2
+        #     if self.color[((self.last_max+1) % 3)] <200:
+        #         self.color[((self.last_max+1) % 3)] += 2
+        #     else:
+        #         self.last_max +=1
+        #         self.last_max %=3
+        # if len(self.top_list):
+        #     print(self.top_list)
+        if self.last_time > self.game.time:
+            self.game.day += 1
+        self.last_time = self.game.time
      
         self.output = f"{self.game.time:02d}:00"
 
@@ -358,6 +468,8 @@ class GameView(arcade.View):
         else:
             if self.stats_open:
                 self.make_stats(event)
+            if self.assign_open:
+                self.make_assign(event)
             self.act_rect_main = Rect(500,400,SCREEN_WIDTH/2,SCREEN_HEIGHT/2, arcade.color.RED_DEVIL)
             self.act_rects.append(self.act_rect_main)
             self.rectangle_appear(self.act_rect_main)
@@ -380,6 +492,7 @@ class GameView(arcade.View):
             
     def act(self, event, key):
         if key == 0:
+            self.time_slept = (6-self.game.time)%24
             self.game.player.sleep((6-self.game.time)%24)
             self.isSleeping = 1
             
@@ -392,12 +505,86 @@ class GameView(arcade.View):
         elif key == 4:
             self.game.scene = 1
         
+
+
         if key != 0:
             self.game.time_passes()
-        self.make_act(event)
+        if key!= 5:
+            self.make_act(event)
+        else:
+            self.make_assign(event)
         self.game.player.cap()
-        arcade.set_background_color(SCENE_COLOR[self.game.scene])
-        
+       
+    def make_assign(self,event):
+        '''
+        self.game.player.do_work()
+        self.time_passes(event)
+        print("doing work")
+        '''
+        def assign_quit(event):
+            for rect in self.assign_rects: # self.stats_rects = list of the ui created by the stats button
+                self.rect_list.remove(rect)
+            self.assign_rects = []
+            for label in self.assign_labels:
+                self.labels.remove(label)
+                self.manager.remove(label)
+            self.assign_labels = []
+            #self.ok_box.remove(self.okButton)
+            self.assign_open = False
+            self.manager.remove(self.gnable)
+            for button in self.assign_buttons:
+                self.assign_buttons_box.remove(button)
+            self.assign_buttons = []
+            
+
+        if self.assign_open:
+            assign_quit(event)
+        else:
+            if self.stats_open:
+                self.make_stats(event)
+            if self.act_open:
+                self.make_act(event)
+            self.assign_rect_main = Rect(500,400,SCREEN_WIDTH/2,SCREEN_HEIGHT/2, arcade.color.GENERIC_VIRIDIAN)
+            self.assign_rects.append(self.assign_rect_main)
+            self.rectangle_appear(self.assign_rect_main)
+            #self.okButton = arcade.gui.UIFlatButton(text = " X ", width = 50)
+            
+            #self.okButton.on_click =  ok_button_quit 
+            #self.ok_box.add(self.okButton)
+            self.assign_open = True
+            
+            self.gnable = arcade.gui.UITextArea(text=" ASSIGNMENTS ", x = SCREEN_WIDTH/3+10, y = 500,
+                                            width=450,
+                                            height=40,
+                                            font_size=20,
+                                            font_name="Kenney Future")
+            
+            
+            
+            self.manager.add(self.gnable)
+            for i in range(len(self.game.cur_assignments)):
+                assignment = self.game.cur_assignments[i]
+                self.assign_buttons.append(self.assignments[assignment])
+                size = len(self.game.cur_assignments)
+                assignmentlabel = arcade.gui.UITextArea(text= f"{assignment} is due {assignment[0]}:00", x = SCREEN_WIDTH*5/9, y = SCREEN_HEIGHT/2 + (i - size/2)*-100,
+                                            width=200,
+                                            height=20,
+                                            font_size=10,
+                                            font_name="Kenney Future")
+            
+                self.assign_labels.append(assignmentlabel)
+                self.labels.append(assignmentlabel)
+                self.manager.add(assignmentlabel)
+                
+
+            for i in range(len(self.assign_buttons)):
+                self.assign_buttons[i] = self.assign_buttons[i].with_space_around(bottom=10)
+                self.assign_buttons_box.add(self.assign_buttons[i])
+
+    def do_assignment(self,event,assignment):
+        self.assignments_done[assignment] = 1
+        self.game.cur_assignments.remove(assignment)
+        self.act(event,5)
 
     def on_draw(self):
         """ Render the screen. """
@@ -408,16 +595,19 @@ class GameView(arcade.View):
         if self.game.teacher_present:
             self.teacher.draw()
         self.lower_frame.draw()
+        
         for n in self.rect_list:
             n.draw()
         
         self.manager.draw()
          # Output the timer text.
+        self.time_frame.draw()
         arcade.draw_text(self.output,
                          30, SCREEN_HEIGHT-20,
                          arcade.color.WHITE, 20,
                          anchor_x="left",
                          anchor_y="top")
+        arcade.set_background_color(TIME_COLORS[self.game.time%3])
         if self.isSleeping:
             if self.sleep_state < 300:
                 Rect(4000,4000, SCREEN_WIDTH, SCREEN_HEIGHT, (0,0,0,self.sleep_state)).draw()
@@ -428,20 +618,8 @@ class GameView(arcade.View):
             else:
                 self.isSleeping = 0
                 self.sleep_state = 0
-                message_box = arcade.gui.UIMessageBox(
-                width=400,
-                height=200,
-                message_text=(
-                    f"You slept __ hours! It is now ___ day!\n"
-                    f"Sleep quality at {self.game.player.stats['sleep']}%"
-                ),
-                callback=self.on_message_box_close,
-                buttons=["Ok", "Cancel"])
-                self.manager.add(message_box)
+                self.create_message(f"You slept {self.time_slept} hours! It is now {DAYS[self.game.day]}!\nSleep quality at {self.game.player.stats['sleep']}%")
             self.sleep_state += 5
-    
-    def on_message_box_close(self, button_text):
-        print(f"User pressed {button_text}.")
 
     def rectangle_appear(self, rectangle):
         # print("Make rectangle")
@@ -453,6 +631,14 @@ class GameView(arcade.View):
         
         self.top_list.append(rectangle)
 
+    def create_message(self, message, button = "Ok"):
+        message_box = arcade.gui.UIMessageBox(
+        width=400,
+        height=200,
+        message_text=message,
+        buttons=[button])
+        self.manager.add(message_box)
+        
 
 
        
--- player.py
+++ player.py
@@ -2,8 +2,18 @@ class Game():
     def __init__(self, name):
         self.player = Player(name)
         self.time = 18
+        self.day = 0
         self.scene = 0
-        self.event_list = [0,0,0,0,0,0, 0,0,1,0,2,0, 0,1,0,2,0,3, 0,0,0,0,0,0]
+        self.event_list = [[0,0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0,0], 
+                           [0,0,0,0,0,0, 0,0,1,0,0,2, 0,1,0,0,2,3, 0,0,0,0,0,0],
+                           [0,0,0,4,0,0, 0,0,1,0,0,2, 0,1,0,0,2,3, 0,0,0,0,0,0], 
+                           [0,0,0,0,0,0, 0,0,1,0,0,2, 0,1,0,0,2,3, 0,0,0,0,0,0],
+                           [0,0,0,0,0,0, 0,0,1,0,0,2, 0,1,0,0,2,3, 0,0,0,0,0,0], 
+                           [0,0,0,0,0,0, 0,0,1,0,0,2, 0,1,0,0,2,3, 0,0,0,0,0,0],
+                           [0,0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0,0, 0,0,0,0,0,0]]
+        self.assignment_list = [(0,20),(1,9),(1,15),(2,10),(2,14),(2,15),(3,14),(4,8),(4,13)]
+        self.test_list = [(3,10),(5,9),(5,15)]
+        self.cur_assignments = []
                         
         self.teacher_present = False
         
@@ -11,11 +21,16 @@ class Game():
     def time_passes(self):
         self.time += 1
         self.time %= 24
-        if self.event_list[self.time] == 1:
+        print(self.day,self.time)
+        
+        if (self.day,self.time) in self.assignment_list:
+            self.cur_assignments.append((self.day,self.time))
+        
+        if self.event_list[self.day][self.time] == 1:
             self.teacher_appear()
-        elif self.event_list[self.time] == 2:
+        elif self.event_list[self.day][self.time] == 2:
             self.teacher_leave()
-        elif self.event_list[self.time] == 3:
+        elif self.event_list[self.day][self.time] == 3:
             self.scene=0
         self.player.time_passes()
 
@@ -58,7 +73,7 @@ class Player():
         self.stats["grade"] = (self.stats["work_ethic"]+self.stats["grade"])/2
     
     def time_passes(self):
-        self.stats["sleep"] -= 5
+        self.stats["sleep"] -= 3
         self.cap()
         self.stats["grade"] -= ((90-self.stats["work_ethic"])+abs((90-self.stats["work_ethic"]))+(90-self.stats["sleep"])+abs((90-self.stats["sleep"])))/12
         self.cap()
