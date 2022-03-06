c9760b9044a9df04ac27b81f79e0807e2a550762
Kimberley Ni <wukimberley98@gmail.com>
2022-03-06 14:40:45 -0500

    Rough Code for Teacher - Apt to Change

--- basic_GUI.py
+++ basic_GUI.py
@@ -657,7 +657,35 @@ class GameView(arcade.View):
         self.manager.add(message_box)
         
 
+class Teacher(arcade.AnimatedWalkingSprite):
+    """ teacher sprite for classroom """
 
-       
+    def __init__(self, pos_x: int, pos_y: int) -> None:
+        super().__init__(center_x=pos_x, center_y=pos_y)
 
-        
+        # Where are the player images stored?
+        appear_texture = "images/teacher.png"
+
+        # Load them all now
+        self.appear_texture = [
+            arcade.load_texture(texture) for texture in walking_texture_path
+        ]
+
+        self.walk_right_textures = [
+            arcade.load_texture(texture, mirrored=True)
+            for texture in walking_texture_path
+        ]
+
+        self.stand_left_textures = [
+            arcade.load_texture(standing_texture_path, mirrored=True)
+        ]
+        self.stand_right_textures = [
+            arcade.load_texture(standing_texture_path)
+        ]
+
+        # Set the enemy defaults
+        self.state = arcade.FACE_LEFT
+        self.change_x = -PLAYER_MOVE_SPEED // 2
+
+        # Set the initial texture
+        self.texture = self.stand_left_textures[0]
